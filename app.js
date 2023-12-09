aa74d1c3a7f0cefb43c9c382a6259abb309fce29
DEARn6sx <nantawats.study@gmail.com>
2023-12-09 13:33:51 +0700

    pull coed form github

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 console.log('Hello');
-=======
 console.log('Hello');
 console.log('Hellozzz');
 console.log('รายงานยอดขาย+++++');
 console.log('รายงานยอดขาย+++++');
->>>>>>> a40099101ff65bf9008a5d042afcc5f229ce1c12
